8af6d3616ede04a6e8531882120b8405e1d4961d
José Angel Lujan Villaseñor <jose.lujan@lemonthree.mx>
2016-10-09 21:55:59 -0600

    Gallery functionality and style

--- page-galeria.php
+++ page-galeria.php
@@ -6,34 +6,26 @@
         <div id="carousel-gallery" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
             <ol class="carousel-indicators">
-                <li data-target="#carousel-galleryc" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-gallery" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-gallery" data-slide-to="1"></li>
-                <li data-target="#carousel-gallery" data-slide-to="2"></li>
             </ol>
 
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
                 <div class="item active">
-                    <img src="..." alt="...">
-                    <div class="carousel-caption">
-                        ...
-                    </div>
+                    <img src="img/backgrounds/galeria.jpg" alt="Foto Galería">
                 </div>
                 <div class="item">
-                    <img src="..." alt="...">
-                    <div class="carousel-caption">
-                        ...
-                    </div>
+                    <img src="img/backgrounds/galeria.jpg" alt="Foto Galería">
                 </div>
-                ...
             </div>
 
             <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <a class="left carousel-control" href="#carousel-gallery" role="button" data-slide="prev">
                 <span class="icon-prev" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <a class="right carousel-control" href="#carousel-gallery" role="button" data-slide="next">
                 <span class="icon-next" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
